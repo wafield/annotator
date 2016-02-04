ac7c8dd6225161ac934d4a06b4cb73f08e2706bc
Ye Tian <wafield.pku@gmail.com>
2016-02-04 17:35:28 -0500

    output script

---
+++ code_freq.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+settings.configure()
+from annotator.models import *
+# resolution categories
+# anno = Annotation.objects.filter(res_code__isnull=False)
+# for cat in ['A1', 'A2', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'C1', 'C2', 'C3', 'C4']:
+#     print cat, anno.filter(res_code=cat).count()
+# anno = Annotation.objects.filter(ref_code__isnull=False)
+# for cat in ['L1', 'L2', 'L3', 'L4', 'G1', 'G2', 'G3', 'G4']:
+#     print cat, anno.filter(res_code=cat).count()
+anno = Annotation.objects.all()
+for an in anno:
+    print '%s|%s|%s' % (an.text, an.res_code, an.ref_code)
+
